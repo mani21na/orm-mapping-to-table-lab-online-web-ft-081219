c216369c6c44b47a2a8d5528d1f1808d5310c061
Hanna Lee <mani21na@gmail.com>
2019-09-24 20:40:26 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
-  attr_accessible :name, :grade
+  attr_accessor :name, :grade
   attr_reader :id 
   
   def initialize (name, grade, id = nil)
@@ -47,4 +47,5 @@ class Student
     student = Student.new(name, grade)
     student.save
     student
+  end
 end
